9d995940a622d70a71d3157a6cdafef7c460bd01
JD Edwards <jd85edwards@gmail.com>
2021-04-01 23:49:35 +0100

    Block broadcasting and Peer node blockchain synchro update

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -4,6 +4,7 @@ import os
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
+from backend.blockchain.block import Block
 
 
 pnconfig = PNConfiguration()
@@ -18,8 +19,22 @@ CHANNELS = {
 
 
 class Listener(SubscribeCallback):
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
+
+        if message_object.channel == CHANNELS['BLOCK']:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print('\n -- Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
 
 
 class PubSub:
@@ -27,10 +42,10 @@ class PubSub:
     Handles the publish/subscribe layer of the application.
     Provides communication between the nodes of the blockchain network.
     """
-    def __init__(self):
+    def __init__(self, blockchain):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener())
+        self.pubnub.add_listener(Listener(blockchain))
 
     def publish(self, channel, message):
         """
