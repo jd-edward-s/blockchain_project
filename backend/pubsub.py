868e95f46e838a8a810ae7d554e5913e7deb6fa8
JD Edwards <jd85edwards@gmail.com>
2021-05-11 12:19:27 +0100

    update to wallet, transaction, and network functionality

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -5,6 +5,7 @@ from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 from backend.blockchain.block import Block
+from backend.wallet.transaction import Transaction
 
 
 pnconfig = PNConfiguration()
@@ -14,13 +15,15 @@ pnconfig.publish_key = os.environ.get("BLOCKCHAIN_PUB_KEY")
 
 CHANNELS = {
     'TEST': 'TEST',
-    'BLOCK': 'BLOCK'
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
 }
 
 
 class Listener(SubscribeCallback):
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
@@ -36,16 +39,21 @@ class Listener(SubscribeCallback):
             except Exception as e:
                 print(f'\n -- Did not replace chain: {e}')
 
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print('\n -- Set the new transaction in the transaction pool')
 
-class PubSub:
+
+class PubSub():
     """
     Handles the publish/subscribe layer of the application.
     Provides communication between the nodes of the blockchain network.
     """
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
 
     def publish(self, channel, message):
         """
@@ -58,6 +66,12 @@ class PubSub:
         Broadcast a block object to all nodes.
         """
         self.publish(CHANNELS['BLOCK'], block.to_json())
+
+    def broadcast_transaction(self, transaction):
+        """
+        Broadcast a transaction to all nodes.
+        """
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
 
 
 def main():
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -8,14 +8,22 @@ class Transaction:
     """
     Document of an exchange in currency from a sender to one or more recipients.
     """
-    def __init__(self, sender_wallet, recipient, amount):
-        self.id = str(uuid.uuid4())[:8]
-        self.output = self.create_output(
+    def __init__(
+            self,
+            sender_wallet=None,
+            recipient=None,
+            amount=None,
+            id=None,
+            output=None,
+            input=None
+    ):
+        self.id = id or str(uuid.uuid4())[:8]
+        self.output = output or self.create_output(
             sender_wallet,
             recipient,
             amount
         )
-        self.input = self.create_input(sender_wallet, self.output)
+        self.input = input or self.create_input(sender_wallet, self.output)
 
     def create_output(self, sender_wallet, recipient, amount):
         """
@@ -32,7 +40,7 @@ class Transaction:
 
     def create_input(self, sender_wallet, output):
         """"
-        Structure the inpot data for the transaction.
+        Structure the input data for the transaction.
         Sign the transaction and include the sender's public key and address.
         """
         return {
@@ -60,10 +68,49 @@ class Transaction:
         # \ is a line continuation trick in python
         self.input = self.create_input(sender_wallet, self.output)
 
+    def to_json(self):
+        """
+        Serialise the transaction.
+        """
+        return self.__dict__
+
+    @staticmethod
+    def from_json(transaction_json):
+        """
+        Deserialize a transaction's json representation back into a Transaction instance.
+        """
+        return Transaction(**transaction_json)
+        # ** is an unpacking method
+        # id=transaction_json['id'],
+        # output=transaction_json['output'],
+        # input=transaction_json['input']
+
+    @staticmethod
+    def is_valid_transaction(transaction):
+        """
+        Validate a transaction.
+        Raise an exception for invalid transactions.
+        """
+        output_total = sum(transaction.output.values())
+
+        if transaction.input['amount'] != output_total:
+            raise Exception('Invalid transaction output values')
+
+        if not Wallet.verify(
+            transaction.input['public_key'],
+            transaction.output,
+            transaction.input['signature']
+        ):
+            raise Exception('Invalid signature')
+
 
 def main():
     transaction = Transaction(Wallet(), 'recipient', 15)
     print(f'transaction.__dict__: {transaction.__dict__}')
+
+    transaction_json = transaction.to_json()
+    restore_transaction = Transaction.from_json(transaction_json)
+    print(f'restored_transaction.__dict__: {restore_transaction.__dict__}')
 
 
 if __name__ == '__main__':
